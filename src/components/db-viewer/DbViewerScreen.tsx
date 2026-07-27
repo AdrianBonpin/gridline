629784d596d0388d180bbf2c788be31e03516394
Adrian Bonpin <adrianbonpin@gmail.com>
2026-07-27 19:54:47 +0800

    fix: use w-0 on right column to force width:0 flex-basis, preventing any content-based expansion

--- src/components/db-viewer/DbViewerScreen.tsx
+++ src/components/db-viewer/DbViewerScreen.tsx
@@ -89,7 +89,7 @@ export function DbViewerScreen({ connectionId, onHome, onSettings }: DbViewerScr
                 <TableTree />
               </div>
             </div>
-            <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
+            <div className="flex-1 w-0 flex flex-col min-w-0 overflow-hidden">
               <TabBar />
               <div className="flex-1 flex flex-col min-h-0 overflow-hidden">
                 <DataGrid />
