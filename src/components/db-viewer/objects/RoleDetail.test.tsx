37e0ad31bcd01d3ed36baabfd734e3b3573cfb51
Adrian Bonpin <adrianbonpin@gmail.com>
2026-08-07 05:33:50 +0800

    chore(test): drop unused fireEvent import in RoleDetail test

--- src/components/db-viewer/objects/RoleDetail.test.tsx
+++ src/components/db-viewer/objects/RoleDetail.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi, beforeEach } from "vitest";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { RoleDetail } from "./RoleDetail";
 import { useDbViewerStore } from "../../../stores/dbViewerStore";
 import * as cmd from "../../../lib/commands";
